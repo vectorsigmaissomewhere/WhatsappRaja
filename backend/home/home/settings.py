c75782104890ad1fac1219a21081eee5a388454c
whereyoucanfindme <anishchy501625@gmail.com>
2025-01-09 10:12:21 +0545

    changes in configuration files

--- backend/home/home/settings.py
+++ backend/home/home/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iob0^^p1_0m)w6(^=rgo&y+ybeiaf6l4+78j=hg-&9r(6=%cdd'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
